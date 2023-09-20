9fd06754676c7e8da177b1221584a6c58df89ad9
stripy1026 <aequatio1026@gmail.com>
2023-09-20 13:44:16 +0900

    feat: :sparkles: add fontawesome package

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,8 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faCircleStop,
+} from "@fortawesome/free-solid-svg-icons";
+
 export const About = () => {
   return (
     <main>
       <span>소개 페이지</span>
-      <div>Name</div>
+      <div>
+        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <FontAwesomeIcon icon={faCircleStop} />
+        <div>조인우</div>
+      </div>
       <div>생년월일</div>
       <div>학력</div>
       <div>Advantages</div>
