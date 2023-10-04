b88912ad79c220a39f84c0a8b1dd4158a702c792
stripy1026 <aequatio1026@gmail.com>
2023-10-05 04:43:56 +0900

    feat: :sparkles: drop random position in sandbox

--- app/utils/abelianSandPile.ts
+++ app/utils/abelianSandPile.ts
@@ -10,22 +10,34 @@ export class AbelianSandpile implements IAbelianSandpile {
   private randomX: number;
   private randomY: number;
 
+  private sandbox: number[][];
+  private unstableList: number[][];
+
   constructor(screenWidth: number, screenHeight: number) {
     this.WIDTH = screenWidth;
     this.HEIGHT = screenHeight;
     this.randomX = 0;
     this.randomY = 0;
+
+    this.sandbox = Array.from(Array(this.WIDTH), () =>
+      Array(this.HEIGHT).fill(0)
+    );
+    this.unstableList = [];
   }
 
   isStable() {
-    return true;
+    return this.unstableList.length === 0;
   }
 
   topple(ctx: CanvasRenderingContext2D) {
-    this.randomX = Math.floor(Math.random() * this.WIDTH);
-    this.randomY = Math.floor(Math.random() * this.HEIGHT);
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(this.randomX, this.randomY, 1, 1);
+    this.randomX = Math.floor((Math.random() * this.WIDTH) / 10);
+    this.randomY = Math.floor((Math.random() * this.HEIGHT) / 10);
+    this.sandbox[this.randomX][this.randomY] += 1;
+    console.log(this.sandbox);
+    ctx.fillStyle = `rgb(${this.sandbox[this.randomX][this.randomY] * 50}, ${
+      this.sandbox[this.randomX][this.randomY] * 50
+    }, ${this.sandbox[this.randomX][this.randomY] * 50})`;
+    ctx.fillRect(this.randomX * 10, this.randomY * 10, 10, 10);
   }
 
   makeAvalanche() {}
