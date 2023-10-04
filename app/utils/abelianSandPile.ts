6492d332c3feec5bc3c8c08a296435fd383e4b74
stripy1026 <aequatio1026@gmail.com>
2023-10-05 05:15:37 +0900

    feat: :construction: working on makeAvalanche

--- app/utils/abelianSandPile.ts
+++ app/utils/abelianSandPile.ts
@@ -1,7 +1,7 @@
 export interface IAbelianSandpile {
   isStable: () => boolean;
   topple: (ctx: CanvasRenderingContext2D) => void;
-  makeAvalanche: () => void;
+  makeAvalanche: (ctx: CanvasRenderingContext2D) => void;
 }
 
 export class AbelianSandpile implements IAbelianSandpile {
@@ -9,6 +9,7 @@ export class AbelianSandpile implements IAbelianSandpile {
   private HEIGHT: number;
   private randomX: number;
   private randomY: number;
+  private randomDistribute: number;
 
   private sandbox: number[][];
   private unstableList: number[][];
@@ -18,11 +19,22 @@ export class AbelianSandpile implements IAbelianSandpile {
     this.HEIGHT = screenHeight;
     this.randomX = 0;
     this.randomY = 0;
+    this.randomDistribute = 0;
 
     this.sandbox = Array.from(Array(this.WIDTH), () =>
       Array(this.HEIGHT).fill(0)
     );
     this.unstableList = [];
+  }
+
+  private drawLevel(
+    ctx: CanvasRenderingContext2D,
+    posX: number,
+    posY: number,
+    level: number
+  ) {
+    ctx.fillStyle = `rgb(${level * 50}, ${level * 50}, ${level * 50})`;
+    ctx.fillRect(posX * 10, posY * 10, 10, 10);
   }
 
   isStable() {
@@ -34,15 +46,31 @@ export class AbelianSandpile implements IAbelianSandpile {
     this.randomY = Math.floor((Math.random() * this.HEIGHT) / 10);
     this.sandbox[this.randomX][this.randomY] += 1;
 
-    ctx.fillStyle = `rgb(${this.sandbox[this.randomX][this.randomY] * 50}, ${
-      this.sandbox[this.randomX][this.randomY] * 50
-    }, ${this.sandbox[this.randomX][this.randomY] * 50})`;
-    ctx.fillRect(this.randomX * 10, this.randomY * 10, 10, 10);
+    this.drawLevel(
+      ctx,
+      this.randomX,
+      this.randomY,
+      this.sandbox[this.randomX][this.randomY]
+    );
 
     if (this.sandbox[this.randomX][this.randomY] > 3) {
       this.unstableList.push([this.randomX, this.randomY]);
     }
   }
 
-  makeAvalanche() {}
+  makeAvalanche(ctx: CanvasRenderingContext2D) {
+    this.randomDistribute = Math.floor(
+      Math.random() * this.unstableList.length
+    );
+    const [topX, topY] = this.unstableList[this.randomDistribute];
+
+    const dx = [topX + 1, topX - 1, topX, topX];
+    const dy = [topY, topY, topY + 1, topY - 1];
+    for (let i = 0; i < 4; ++i) {
+      if (dx[i] < 0 || dx[i] >= this.randomX * 10) continue;
+      if (dy[i] < 0 || dy[i] >= this.randomX * 10) continue;
+      this.sandbox[dx[i]][dy[i]] += 1;
+      this.drawLevel(ctx, dx[i], dy[i], this.sandbox[dx[i]][dy[i]]);
+    }
+  }
 }
