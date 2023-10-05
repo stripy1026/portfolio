386d1c007d68c24a4d62df45ee5aad4916ef7270
stripy1026 <aequatio1026@gmail.com>
2023-10-05 23:49:31 +0900

    style: :lipstick: move text into background

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -11,25 +11,24 @@ export const Intro = () => {
 
   return (
     <main ref={mainRef}>
-      <BackgroundAnimation width={width} height={height} />
-      <section>
-        <div>
+      <section className="relative">
+        <BackgroundAnimation width={width} height={height} />
+      </section>
+      <div className="relative bottom-full">
+        <section className="flex flex-col items-center">
           <h1>랜딩 페이지 애니메이션</h1>
           <h1>Abelian Sandpile Model</h1>
           <h2>졸업논문을 위해 공부했던 모델을 조금 변형해서 구현했습니다</h2>
-        </div>
-      </section>
-      <section>
-        <h2>조인우</h2>
-        <h2>프론트엔드 개발자</h2>
-      </section>
-      <section>
-        <div>
+        </section>
+        <section className="flex flex-col items-center">
+          <h2>조인우</h2>
+          <h2>프론트엔드 개발자</h2>
+        </section>
+        <section className="flex flex-col items-center">
           <h1>저는 아무것도 모릅니다</h1>
           <h1>그렇기에, 모든 것을 배울 준비가 되어 있습니다</h1>
-        </div>
-      </section>
-
+        </section>
+      </div>
       <Link href="#about">포트폴리오 확인</Link>
     </main>
   );
