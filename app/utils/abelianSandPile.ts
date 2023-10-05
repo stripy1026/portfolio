a570db314d49cdabbcb033b74fba9f9c79854795
stripy1026 <aequatio1026@gmail.com>
2023-10-05 23:22:34 +0900

    feat: :sparkles: sandpile parameters done

--- app/utils/abelianSandPile.ts
+++ app/utils/abelianSandPile.ts
@@ -1,3 +1,14 @@
+const TOPPLE_SIZE = 1;
+const INITIAL_TOPPLE_VELOCITY = 2000;
+const MINIMUM_TOPPLE_VELOCITY = 10;
+const BRIGHT_RATIO = 10;
+const TOPPLE_WEIGHT = 1;
+const CRITICAL_LEVEL = 8;
+const MAXIMUM_EXPLODE_RANGE = 4;
+const BURST_RATIO = 2;
+
+// INITIAL TOPPLE VELOCITY must be changed depends on screen size
+
 export interface IAbelianSandpile {
   isStable: () => boolean;
   topple: (ctx: CanvasRenderingContext2D) => void;
@@ -14,12 +25,16 @@ export class AbelianSandpile implements IAbelianSandpile {
   private sandbox: number[][];
   private unstableList: number[][];
 
+  private toppleVelocity: number;
+
   constructor(screenWidth: number, screenHeight: number) {
     this.WIDTH = screenWidth;
     this.HEIGHT = screenHeight;
     this.randomX = 0;
     this.randomY = 0;
     this.randomDistribute = 0;
+
+    this.toppleVelocity = INITIAL_TOPPLE_VELOCITY;
 
     this.sandbox = Array.from(Array(this.WIDTH), () =>
       Array(this.HEIGHT).fill(0)
@@ -37,9 +52,16 @@ export class AbelianSandpile implements IAbelianSandpile {
     if (level < 0) {
       ctx.fillStyle = "rgb(0, 0, 0)";
     } else {
-      ctx.fillStyle = `rgb(${level * 50}, ${level * 50}, ${level * 50})`;
+      ctx.fillStyle = `rgb(${level * BRIGHT_RATIO}, ${level * BRIGHT_RATIO}, ${
+        level * BRIGHT_RATIO
+      })`;
     }
-    ctx.fillRect(posX * 10, posY * 10, 10, 10);
+    ctx.fillRect(
+      posX * TOPPLE_SIZE,
+      posY * TOPPLE_SIZE,
+      TOPPLE_SIZE,
+      TOPPLE_SIZE
+    );
   }
 
   isStable() {
@@ -47,26 +69,30 @@ export class AbelianSandpile implements IAbelianSandpile {
   }
 
   topple(ctx: CanvasRenderingContext2D) {
-    this.randomX = Math.floor((Math.random() * this.WIDTH) / 10);
-    this.randomY = Math.floor((Math.random() * this.HEIGHT) / 10);
-
-    if (!this.sandbox[this.randomX]) {
-      return;
+    if (this.toppleVelocity > MINIMUM_TOPPLE_VELOCITY) {
+      this.toppleVelocity -= 1;
     }
+    for (let i = 0; i < this.toppleVelocity; ++i) {
+      this.randomX = Math.floor((Math.random() * this.WIDTH) / TOPPLE_SIZE);
+      this.randomY = Math.floor((Math.random() * this.HEIGHT) / TOPPLE_SIZE);
 
-    this.sandbox[this.randomX][this.randomY] += 1;
+      if (!this.sandbox[this.randomX]) {
+        return;
+      }
 
-    this.drawLevel(
-      ctx,
-      this.randomX,
-      this.randomY,
-      this.sandbox[this.randomX][this.randomY]
-    );
+      this.sandbox[this.randomX][this.randomY] += TOPPLE_WEIGHT;
 
-    if (this.sandbox[this.randomX][this.randomY] >= 4) {
-      this.unstableList.push([this.randomX, this.randomY]);
+      this.drawLevel(
+        ctx,
+        this.randomX,
+        this.randomY,
+        this.sandbox[this.randomX][this.randomY]
+      );
+
+      if (this.sandbox[this.randomX][this.randomY] >= CRITICAL_LEVEL) {
+        this.unstableList.push([this.randomX, this.randomY]);
+      }
     }
-    console.log("topple");
   }
 
   makeAvalanche(ctx: CanvasRenderingContext2D) {
@@ -75,20 +101,47 @@ export class AbelianSandpile implements IAbelianSandpile {
     );
     const [topX, topY] = this.unstableList[this.randomDistribute];
 
-    const dx = [topX + 1, topX - 1, topX, topX];
-    const dy = [topY, topY, topY + 1, topY - 1];
-    for (let i = 0; i < 4; ++i) {
-      if (dx[i] < 0 || dx[i] >= this.randomX * 10) continue;
-      if (dy[i] < 0 || dy[i] >= this.randomX * 10) continue;
-      this.sandbox[dx[i]][dy[i]] += 4;
-      this.drawLevel(ctx, dx[i], dy[i], this.sandbox[dx[i]][dy[i]]);
-      if (this.sandbox[dx[i]][dy[i]] >= 4) {
-        this.unstableList.push([dx[i], dy[i]]);
+    for (
+      let explodeRange = 1;
+      explodeRange < MAXIMUM_EXPLODE_RANGE;
+      ++explodeRange
+    ) {
+      const dx = [
+        topX + explodeRange,
+        topX - explodeRange,
+        topX,
+        topX,
+        topX + explodeRange,
+        topX + explodeRange,
+        topX - explodeRange,
+        topX - explodeRange,
+      ];
+      const dy = [
+        topY,
+        topY,
+        topY + explodeRange,
+        topY - explodeRange,
+        topY + explodeRange,
+        topY - explodeRange,
+        topY + explodeRange,
+        topY - explodeRange,
+      ];
+      for (let i = 0; i < 8; ++i) {
+        if (dx[i] < 0 || dx[i] >= this.WIDTH * TOPPLE_SIZE) continue;
+        if (dy[i] < 0 || dy[i] >= this.HEIGHT * TOPPLE_SIZE) continue;
+        if (!this.sandbox[dx[i]]) {
+          return;
+        }
+        this.sandbox[dx[i]][dy[i]] += CRITICAL_LEVEL * BURST_RATIO;
+        this.drawLevel(ctx, dx[i], dy[i], this.sandbox[dx[i]][dy[i]]);
+        if (this.sandbox[dx[i]][dy[i]] >= CRITICAL_LEVEL) {
+          this.unstableList.push([dx[i], dy[i]]);
+        }
       }
     }
-    this.sandbox[topX][topY] -= 20;
+    this.sandbox[topX][topY] -=
+      CRITICAL_LEVEL * MAXIMUM_EXPLODE_RANGE * BURST_RATIO * 16;
     this.unstableList.splice(this.randomDistribute, 1);
     this.drawLevel(ctx, topX, topY, this.sandbox[topX][topY]);
-    console.log("avalanche");
   }
 }
