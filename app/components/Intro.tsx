2dfbfff87562f8c6bd09d75158bd9166c67f84cf
stripy1026 <aequatio1026@gmail.com>
2023-10-05 23:26:22 +0900

    feat: :sparkles: abelian model introduced

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -14,14 +14,22 @@ export const Intro = () => {
       <BackgroundAnimation width={width} height={height} />
       <section>
         <div>
-          <h1>저는 아무것도 모릅니다</h1>
-          <h1>그렇기에, 모든 것을 배울 준비가 되어 있습니다</h1>
+          <h1>랜딩 페이지 애니메이션</h1>
+          <h1>Abelian Sandpile Model</h1>
+          <h2>졸업논문을 위해 공부했던 모델을 조금 변형해서 구현했습니다</h2>
         </div>
       </section>
       <section>
         <h2>조인우</h2>
         <h2>프론트엔드 개발자</h2>
       </section>
+      <section>
+        <div>
+          <h1>저는 아무것도 모릅니다</h1>
+          <h1>그렇기에, 모든 것을 배울 준비가 되어 있습니다</h1>
+        </div>
+      </section>
+
       <Link href="#about">포트폴리오 확인</Link>
     </main>
   );
