13e71c0f988203902830e226cd4e8785b2885231
stripy1026 <aequatio1026@gmail.com>
2023-10-07 22:45:51 +0900

    feat: :sparkles: contact message done
    need to connect DB

--- app/components/MessageForm.tsx
+++ app/components/MessageForm.tsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+
 export const MessageForm = () => {
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [message, setMessage] = useState("");
+  const [isOK, setIsOK] = useState(true);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const submitMessage = () => {
+    if (!name || !title || !message) {
+      setIsOK(false);
+      setIsSubmit(false);
+      return;
+    }
+    console.log(name, title, message);
+    setIsOK(true);
+    setIsSubmit(true);
+  };
+
   return (
     <div className="my-10">
       <div className="flex mb-2">
@@ -9,6 +32,8 @@ export const MessageForm = () => {
           className="bg-gray-900 min-w-[344px] px-2"
           maxLength={80}
           placeholder="80자 이하"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </div>
       <div className="flex mb-2">
@@ -19,15 +44,35 @@ export const MessageForm = () => {
           className="bg-gray-900 min-w-[344px] px-2"
           maxLength={80}
           placeholder="80자 이하"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
       <textarea
         className="bg-gray-900 min-w-[400px] min-h-[200px] p-2"
         placeholder="메세지를 입력해주세요"
         maxLength={1000}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
       />
       <div className="flex justify-end">
-        <button className="border-2 p-2 border-gray-700 text-rose-700">
+        {!isOK && (
+          <span className="flex items-center mx-5 text-rose-700">
+            제출에 실패했습니다
+          </span>
+        )}
+        {isSubmit && (
+          <div className="flex items-center mr-5">
+            <FontAwesomeIcon
+              className="text-2xl text-green-700"
+              icon={faCheck}
+            />
+          </div>
+        )}
+        <button
+          className="border-2 p-2 border-gray-700 text-rose-700"
+          onClick={submitMessage}
+        >
           SUBMIT
         </button>
       </div>
