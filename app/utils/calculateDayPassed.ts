0f2d8d2d328a5037b7be3a6570e8686122944749
stripy1026 <aequatio1026@gmail.com>
2023-10-07 11:55:44 +0900

    fix: :bug: today getmonth + 1

--- app/utils/calculateDayPassed.ts
+++ app/utils/calculateDayPassed.ts
@@ -1,6 +1,11 @@
 export const calculateDayPassed = (startDay: Date) => {
   const today = new Date();
-  const diffMsec = today.getTime() - startDay.getTime();
+  const modifiedDay = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    today.getDate()
+  );
+  const diffMsec = modifiedDay.getTime() - startDay.getTime();
   const day = Math.floor(diffMsec / (1000 * 60 * 60 * 24));
   return day;
 };
