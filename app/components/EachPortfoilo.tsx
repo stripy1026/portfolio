105d145d94f4c312acda723c5a1a499b76de89f4
stripy1026 <aequatio1026@gmail.com>
2023-10-09 03:44:34 +0900

    feat: :sparkles: eachportfolio style done
    server running out of memory need to fix

--- app/components/EachPortfoilo.tsx
+++ app/components/EachPortfoilo.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 type EachPortfolioProps = {
   imageSrc: string;
@@ -11,19 +14,30 @@ export const EachPortfoilo = ({
   title,
   language,
 }: EachPortfolioProps) => {
-  // 대문사진, 제목, 개발언어 prop
   // 개별 페이지 이동 버튼
   const IMAGE_WIDTH = 400;
-  const IMAGE_HEIGHT = (IMAGE_WIDTH * 9) / 16;
+  const IMAGE_HEIGHT = (IMAGE_WIDTH * 3) / 4;
+
+  const [isDetail, setIsDetail] = useState(false);
 
   return (
-    <div>
-      <Image
-        src={imageSrc}
-        alt={title}
-        width={IMAGE_WIDTH}
-        height={IMAGE_HEIGHT}
-      />
-    </div>
+    <button onClick={() => setIsDetail((prev) => !prev)}>
+      {isDetail ? (
+        <div className="w-[400px] h-[318px] flex flex-col items-center border-dashed border-2 border-rose-700">
+          <span className="text-2xl font-bold mt-20">{title}</span>
+          <span className="text-rose-600">{language}</span>
+          <button className="border-2 p-2 border-slate-600 text-rose-700 mt-20">
+            SEE MORE
+          </button>
+        </div>
+      ) : (
+        <Image
+          src={imageSrc}
+          alt={title}
+          width={IMAGE_WIDTH}
+          height={IMAGE_HEIGHT}
+        />
+      )}
+    </button>
   );
 };
