c93955078a7d66464cc08bc0c94436dcf7759ff4
stripy1026 <aequatio1026@gmail.com>
2023-10-09 01:03:36 +0900

    feat: :sparkles: login works

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,26 @@
 import { Mail } from "@/components/Mail";
 import { getMessages } from "lib/messages-db";
+import Link from "next/link";
 
-export default async function Admin() {
+export default async function Admin({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const { messages } = await getMessages();
-
   messages?.reverse();
 
   return (
     <div className="min-h-screen">
-      <span>ADMIN PAGE</span>
-      {messages?.map((mail) => (
-        <Mail key={mail.id} mail={mail} createdAt={mail.createdAt} />
-      ))}
+      <h1>ADMIN PAGE</h1>
+      <Link href="/">GO HOME</Link>
+      {searchParams?.password === process.env.ADMIN_PASSWORD ? (
+        messages?.map((mail) => (
+          <Mail key={mail.id} mail={mail} createdAt={mail.createdAt} />
+        ))
+      ) : (
+        <p>INCORRECT PASSWORD</p>
+      )}
     </div>
   );
 }
