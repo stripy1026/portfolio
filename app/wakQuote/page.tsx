32b5b0e3331e1f87a5133c3ff7d654798f45bd83
stripy1026 <aequatio1026@gmail.com>
2023-10-09 23:47:27 +0900

    feat: :sparkles: quote template text done

--- app/wakQuote/page.tsx
+++ app/wakQuote/page.tsx
@@ -42,6 +42,18 @@ export default function WakQuote() {
             alt="template"
             fill={true}
           />
+          <div className="relative left-[60%] w-[225px] mt-5">
+            <div className="flex flex-col items-center">
+              <p className="text-xl font-bold">개발자 (1명)</p>
+              <p>조인우</p>
+              <p className="text-xl font-bold mt-10">개발 언어</p>
+              <p>Next.js (Page router)</p>
+              <p className="mt-10">
+                트위치 스트리머 우왁굳의 명언을 자유롭게 기록하고 열람할 수 있는
+                사이트입니다.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </main>
