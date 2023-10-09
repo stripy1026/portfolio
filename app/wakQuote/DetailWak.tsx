35eeba3d92b2bd3fd06db449466adcec73a6af5a
stripy1026 <aequatio1026@gmail.com>
2023-10-10 01:28:08 +0900

    feat: :sparkles: wakquote done

--- app/wakQuote/DetailWak.tsx
+++ app/wakQuote/DetailWak.tsx
@@ -4,79 +4,109 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import { useState } from "react";
+import { RegretWak } from "./RegretWak";
 
 export const DetailWak = () => {
   const [scrollNumber, setScrollNumber] = useState(0);
+  const [isImg, setIsImg] = useState(true);
   return (
     <div>
-      <div className="flex my-10 xl:hidden">
-        <button
-          className="flex items-end mx-1"
-          onClick={() => {
-            if (scrollNumber === 0) {
-              setScrollNumber(2);
-            } else {
-              setScrollNumber((prev) => prev - 1);
-            }
-          }}
-        >
-          <FontAwesomeIcon
-            className="text-3xl p-2 bg-gray-300 text-blue-700"
-            icon={faArrowLeft}
-          />
+      <div className="xl:hidden my-5 flex justify-center">
+        <button onClick={() => setIsImg(true)}>
+          <span className="mr-10 underline">이미지</span>
         </button>
+        <button onClick={() => setIsImg(false)}>
+          <span className="underline">후회의 기록</span>
+        </button>
+      </div>
+      {isImg ? (
         <div>
-          {scrollNumber === 0 && (
-            <div className="relative w-[300px] h-[220px]">
+          <div className="flex mb-10 xl:hidden">
+            <button
+              className="flex items-end mx-1"
+              onClick={() => {
+                if (scrollNumber === 0) {
+                  setScrollNumber(2);
+                } else {
+                  setScrollNumber((prev) => prev - 1);
+                }
+              }}
+            >
+              <FontAwesomeIcon
+                className="text-3xl p-2 bg-gray-300 text-blue-700"
+                icon={faArrowLeft}
+              />
+            </button>
+            <div>
+              {scrollNumber === 0 && (
+                <div className="relative w-[300px] h-[220px]">
+                  <Image
+                    src="/wakQuote/wakQuote_main.png"
+                    alt="img1"
+                    fill={true}
+                  />
+                </div>
+              )}
+              {scrollNumber === 1 && (
+                <div className="relative w-[300px] h-[220px]">
+                  <Image
+                    src="/wakQuote/wakQuote_ranking.png"
+                    alt="img2"
+                    fill={true}
+                  />
+                </div>
+              )}
+              {scrollNumber === 2 && (
+                <div className="relative w-[300px] h-[220px]">
+                  <Image
+                    src="/wakQuote/wakQuote_profile.png"
+                    alt="img3"
+                    fill={true}
+                  />
+                </div>
+              )}
+            </div>
+            <button
+              className="flex items-end mx-1"
+              onClick={() => {
+                if (scrollNumber === 2) {
+                  setScrollNumber(0);
+                } else {
+                  setScrollNumber((prev) => prev + 1);
+                }
+              }}
+            >
+              <FontAwesomeIcon
+                className="text-3xl p-2 bg-gray-300 text-blue-700"
+                icon={faArrowRight}
+              />
+            </button>
+          </div>
+          <div className="max-[1279px]:hidden my-10 flex">
+            <div className="relative w-[300px] h-[220px] mx-5">
               <Image src="/wakQuote/wakQuote_main.png" alt="img1" fill={true} />
             </div>
-          )}
-          {scrollNumber === 1 && (
-            <div className="relative w-[300px] h-[220px]">
+            <div className="relative w-[300px] h-[220px] mx-5">
               <Image
                 src="/wakQuote/wakQuote_ranking.png"
                 alt="img2"
                 fill={true}
               />
             </div>
-          )}
-          {scrollNumber === 2 && (
-            <div className="relative w-[300px] h-[220px]">
+            <div className="relative w-[300px] h-[220px] mx-5">
               <Image
                 src="/wakQuote/wakQuote_profile.png"
                 alt="img3"
                 fill={true}
               />
             </div>
-          )}
+          </div>
         </div>
-        <button
-          className="flex items-end mx-1"
-          onClick={() => {
-            if (scrollNumber === 2) {
-              setScrollNumber(0);
-            } else {
-              setScrollNumber((prev) => prev + 1);
-            }
-          }}
-        >
-          <FontAwesomeIcon
-            className="text-3xl p-2 bg-gray-300 text-blue-700"
-            icon={faArrowRight}
-          />
-        </button>
-      </div>
-      <div className="max-[1279px]:hidden my-10 flex">
-        <div className="relative w-[300px] h-[220px] mx-5">
-          <Image src="/wakQuote/wakQuote_main.png" alt="img1" fill={true} />
+      ) : (
+        <div className="relative -left-10 mb-10">
+          <RegretWak />
         </div>
-        <div className="relative w-[300px] h-[220px] mx-5">
-          <Image src="/wakQuote/wakQuote_ranking.png" alt="img2" fill={true} />
-        </div>
-        <div className="relative w-[300px] h-[220px] mx-5">
-          <Image src="/wakQuote/wakQuote_profile.png" alt="img3" fill={true} />
-        </div>
-      </div>
+      )}
     </div>
   );
 };
--- app/wakQuote/page.tsx
+++ app/wakQuote/page.tsx
@@ -4,10 +4,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
 import { DetailWak } from "./DetailWak";
+import { RegretWak } from "./RegretWak";
 
 export default function WakQuote() {
   return (
-    <main className="h-screen">
+    <main>
       <section className="w-full bg-gray-900 py-5 pl-20 flex justify-between items-center">
         <div className="flex">
           <Link
@@ -37,7 +38,7 @@ export default function WakQuote() {
       </section>
       <section className="flex flex-col items-center mt-10">
         <h1 className="font-black text-5xl mb-10">우왁굳 명언 아카이브</h1>
-        <div className="flex">
+        <div className="flex max-[640px]:hidden">
           <div className="relative w-[640px] h-[360px]">
             <Image
               src="/wakQuote/quote_template.png"
@@ -57,24 +58,19 @@ export default function WakQuote() {
               </div>
             </div>
           </div>
-          <div className="max-[1279px]:hidden ml-20 flex flex-col items-center  max-w-[400px] p-10 border-2 border-dotted">
-            <p className="text-2xl font-bold mb-5">후회의 기록</p>
-            <p>
-              나의 첫 웹 페이지를 만드는 팀 프로젝트가 끝나고, 처음으로 진행했던
-              토이 프로젝트.
-            </p>
-            <p>
-              &nbsp;풀스택 웹 페이지를 만들어보고 싶다는 일념만으로 진행했던
-              프로젝트였다.
-            </p>
-            <p>
-              &nbsp;혼자서 웹 페이지를 만드는 것이 처음이었어서, 리팩토링 하고픈
-              아쉬운 부분이 많다.
-            </p>
-            <p>
-              &nbsp;사실상 처음으로 CSS를 혼자서 만들었는데, 그땐 CSS를 다루기가
-              너무 어려워 chatGPT에 거의 의존했었고, 그럼에도 미비한 디자인이
-              너무 아쉬웠던 기억이 있다.
+          <div className="max-[1279px]:hidden">
+            <RegretWak />
+          </div>
+        </div>
+        <div className="min-[641px]:hidden w-[225px] my-4">
+          <div className="flex flex-col items-center">
+            <p className="text-xl font-bold">개발자 (1명)</p>
+            <p>조인우</p>
+            <p className="text-xl font-bold mt-10">개발 언어</p>
+            <p>Next.js (Page router)</p>
+            <p className="mt-10">
+              트위치 스트리머 우왁굳의 명언을 자유롭게 기록하고 열람할 수 있는
+              사이트입니다.
             </p>
           </div>
         </div>
