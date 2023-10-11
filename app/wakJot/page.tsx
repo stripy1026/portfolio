1550f8d5f9b4f7887671cf6a4f73f37b8b7fd2a1
stripy1026 <aequatio1026@gmail.com>
2023-10-11 21:15:43 +0900

    feat: :sparkles: wakjot WIP page done

--- app/wakJot/page.tsx
+++ app/wakJot/page.tsx
@@ -1,7 +1,23 @@
+import { faHelmetSafety } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function WakJot() {
   return (
     <main className="h-screen">
-      <div>WakJot page</div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="mt-20 flex flex-col items-center">
+          <p className="text-5xl font-black">WAKJOT</p>
+          <FontAwesomeIcon
+            className="text-9xl my-10 text-yellow-400"
+            icon={faHelmetSafety}
+          />
+          <p>버전 1.0.0</p>
+          <p>2023년 9월 5일 개발 시작</p>
+          <p>2023년 10월 2일 개발 완료</p>
+          <p>버전 2.0.0</p>
+          <p>현재 개발 진행중</p>
+        </div>
+      </div>
     </main>
   );
 }
