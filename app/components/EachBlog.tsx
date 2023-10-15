b2cafe70da22d37bf25d1e5d48265e537a33f0c5
stripy1026 <aequatio1026@gmail.com>
2023-10-16 07:06:36 +0900

    feat: :sparkles: blog post design done

--- app/components/EachBlog.tsx
+++ app/components/EachBlog.tsx
@@ -22,14 +22,14 @@ export const EachBlog = ({
   const animateItem = useScrollFadeIn();
 
   return (
-    <div className="h-[240px] sm:h-[320px]" {...animateItem}>
+    <div className="h-[240px] sm:h-[320px] overflow-hidden" {...animateItem}>
       <Link href={href} target="_blank" rel="noreferrer">
         <div className="w-[300px] h-[120px] sm:w-[400px] sm:h-[160px] relative">
           <Image src={imageSrc} alt={title} fill={true} />
         </div>
         <h3 className="mt-2 font-bold text-lg">{title}</h3>
-        <p className="text-sm">{createdDate}</p>
-        <p className="my-2">{description}</p>
+        <p className="text-xs">{createdDate}</p>
+        <p className="text-sm my-2">{description}</p>
       </Link>
     </div>
   );
